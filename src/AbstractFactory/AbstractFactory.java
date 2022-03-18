2f29eadca7b18449cd0ba395b10f7a5da70cb133
defr0stt <dima.sb713@gmail.com>
2022-03-18 18:05:17 +0200

    Added description to abstract factory

--- src/AbstractFactory/AbstractFactory.java
+++ src/AbstractFactory/AbstractFactory.java
@@ -1,5 +1,18 @@
 package AbstractFactory;
 
+// It is the generating pattern of design that allows you
+// to create families of related objects without
+// being tied to specific classes of created objects.
+//
+// Advantages :
+//      - Frees client code from binding to specific product classes.
+//      - Allocates product production code in one place, simplifying code support.
+//      - Simplifies adding new products to the program.
+//
+// Disadvantages :
+//      - Complicates the program code due to the introduction of
+//          a large number of additional classes.
+
 public interface AbstractFactory
 {
     public abstract VideoCard createVideoCard();
