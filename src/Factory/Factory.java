7ee211d10af7bf6af9d15ba1679928b6ad9f359a
defr0stt <dima.sb713@gmail.com>
2022-03-18 23:23:26 +0200

    Description to factory method

--- src/Factory/Factory.java
+++ src/Factory/Factory.java
@@ -1,5 +1,22 @@
 package Factory;
 
+// It is a generative pattern design, which
+// defines a common interface for creating objects
+// in a superclass, allowing subclasses
+// change the type of objects being created.
+//
+// Advantages :
+//      - Relieves the class from being tied to specific classes
+//          products.
+//      - Allocates the code of production of products in one place, simplifies
+//          code support.
+//      - Simplifies adding new products to the program.
+//
+// Disadvantages :
+//      - Can lead to the creation of large parallel hierarchies
+//          classes for each product class requires the creation of
+//          dig your own subclass of the creator.
+
 public abstract class Factory
 {
     public void transportWay(Transport transport){
