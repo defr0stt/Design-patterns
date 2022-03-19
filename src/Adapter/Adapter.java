127a09ee7ba006758fb43f1cb5cb8d5292d01abb
defr0stt <dima.sb713@gmail.com>
2022-03-19 18:33:04 +0200

    Fixed adapter

--- src/Adapter/Adapter.java
+++ src/Adapter/Adapter.java
@@ -10,34 +10,39 @@ package Adapter;
 // Complicates the program code due to the adding of additional
 // classes.
 
+import static java.lang.System.out;
+
 public class Adapter
 {
+    private Adapter(){}
+
     static public boolean checkEngine(Engine engine, int min, int max){
         if(engine.getPower() >= min && engine.getPower() <= max){
-            System.out.println(engine + " fits to this type of vehicle");
+            out.println(engine + " fits to this type of vehicle");
             return true;
         } else {
-            System.out.println(engine + " doesn't fit to this type of vehicle");
+            out.println(engine + " doesn't fit to this type of vehicle");
             return false;
         }
     }
 
     static public void adaptEngine(Vehicle vehicle, Engine engine, boolean put){
+        String thisEngine = "This engine : ";
         if(vehicle.getMin() <= engine.getPower() && vehicle.getMax() >= engine.getPower()){
-            System.out.println("This engine : " + engine + " not required to be adapted");
+            out.println(thisEngine + engine + " not required to be adapted");
             if(put){
                 vehicle.setEngine(engine);
             }
         } else {
             Engine tempEngine = put ? new Engine(engine) : null;
             if(vehicle.getMin() > engine.getPower()) {
-                System.out.println("This engine : " + engine + " adapted to minimum " + vehicle.getMin() + "HP");
+                out.println(thisEngine + engine + " adapted to minimum " + vehicle.getMin() + "HP");
                 if(put){
                     tempEngine.setPower(vehicle.getMin());
                     vehicle.setEngine(tempEngine);
                 }
             } else if(vehicle.getMax() < engine.getPower()) {
-                System.out.println("This engine : " + engine + " adapted to maximum " + vehicle.getMax() + "HP");
+                out.println(thisEngine + engine + " adapted to maximum " + vehicle.getMax() + "HP");
                 if(put){
                     tempEngine.setPower(vehicle.getMax());
                     vehicle.setEngine(tempEngine);
